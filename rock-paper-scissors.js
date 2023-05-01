b67f3ef76b20d5427170bc8c172410ea15b61b43
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-05-01 12:53:43 +0200

    feat: implement better decision-making for cpu
    Created the functions, playDefensive, playAggressive, alwaysCounter,
    and playRandom, that will be randomly chosen to execute by the cpu
    in the getComputerChoice function. This allows the cpu to make less
    predictable choices, while attempting to predict to player's choice
    based on the history of the choices they've made before.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -9,22 +9,12 @@ const computerScoreDisp = document.querySelector('#computer-score');
 
 function getComputerChoice() {
 
-	let computerChoice;
-
-  if (playerChoiceHistory.length === 0) {
-    return CHOICES[Math.floor(Math.random() * 3)];
-  } else {
-    const choiceMode = mode(playerChoiceHistory);
-    if (choiceMode === 'rock') {
-      return 'paper';
-    } else if (choiceMode === 'paper') {
-      return 'scissors';
-    } else if (choiceMode === 'scissors') {
-      return 'rock';
-    }
-  }
-
-	return computerChoice;
+  const strategies = [playDefensive, playAggressive, alwaysCounter, playRandom];
+  const randomIndex = Math.floor(Math.random() * strategies.length);
+  const lastChoice = playerChoiceHistory[playerChoiceHistory.length - 1];
+  return (playerChoiceHistory.length > 0) ?
+  strategies[randomIndex](lastChoice) :
+  CHOICES[Math.floor(Math.random() * 3)];
 
 }
 
@@ -102,23 +92,105 @@ function determineGameWinner() {
   }
 }
 
-function mode(array) {
-    if(array.length == 0)
-        return null;
-    let modeMap = {};
-    let maxEl = array[0], maxCount = 1;
-    for(let i = 0; i < array.length; i++)
-    {
-        let el = array[i];
-        if(modeMap[el] == null)
-            modeMap[el] = 1;
-        else
-            modeMap[el]++;  
-        if(modeMap[el] > maxCount)
-        {
-            maxEl = el;
-            maxCount = modeMap[el];
-        }
+function findMostCommon(arr) {
+  let freqMap = {};
+  let mostCommon = arr[0];
+
+  arr.forEach(function (elem) {
+    if (freqMap[elem]) {
+      freqMap[elem]++;
+    } else {
+      freqMap[elem] = 1;
     }
-    return maxEl;
+    if (freqMap[elem] > freqMap[mostCommon]) {
+      mostCommon = elem;
+    }
+  });
+
+  return mostCommon;
+}
+
+const counterMoves = {
+  rock: ['rock', 'paper', 'scissors'],
+  paper: ['paper', 'scissors', 'rock'],
+  scissors: ['scissors', 'rock', 'paper'],
+}
+
+function playDefensive(playerChoice) {
+  console.log('Please don\'t bully me');
+  const randomIndex = Math.floor(Math.random() * 2);
+  const mostCommon = findMostCommon(playerChoiceHistory);
+  const choicesCount = playerChoiceHistory.reduce((choicesCount, choice) => {
+    choicesCount[choice] = ++choicesCount[choice] || 1;
+
+    return choicesCount;
+  }, {});
+
+  if (choicesCount[mostCommon] > 1 && computerScore >= playerScore) {
+    return counterMoves[playerChoice][0];
+  } else {
+    return counterMoves[playerChoice][randomIndex];
+  }
+}
+
+function playAggressive(playerChoice) {
+  console.log('Get fucked you nerd.');
+  const randomIndex = Math.floor(Math.random() * 2);
+  const cpuChoiceHistory = playerChoiceHistory.slice(1);
+  const playerLastChoice = playerChoiceHistory[playerChoiceHistory.length - 1];
+  const cpuLastChoice = cpuChoiceHistory[cpuChoiceHistory.length - 1];
+  const playerScoreDiff = playerScore - computerScore;
+  let choiceIndex;
+
+  if (isPlayerAggressive(playerChoiceHistory)) {
+    if (playerScoreDiff > 0) {
+      choiceIndex = 1;
+    } else if (playerScoreDiff < 0) {
+      choiceIndex = 0;
+    } else {
+      choiceIndex = randomIndex + 1;
+    }
+  } else {
+    if (cpuLastChoice === playerLastChoice) {
+      choiceIndex = randomIndex + 1;
+    } else {
+      const counterMoveIndex = counterMoves[playerLastChoice].indexOf(cpuLastChoice);
+      choiceIndex = (counterMoveIndex + 2) % 3;
+    }
+  }
+
+  return counterMoves[playerChoice][choiceIndex];
+}
+
+function alwaysCounter(playerChoice) {
+  console.log('Get countered.')
+  const mostCommon = findMostCommon(playerChoiceHistory);
+  const choicesCount = playerChoiceHistory.reduce((choicesCount, choice) => {
+    choicesCount[choice] = ++choicesCount[choice] || 1;
+
+    return choicesCount;
+  }, {});
+
+  if (choicesCount[mostCommon] > 1) {
+    return counterMoves[playerChoice][1];
+  } else {
+    return counterMoves[playerChoice][0];
+  }
+}
+
+function isPlayerAggressive(arr) {
+  const mostCommon = findMostCommon(playerChoiceHistory);
+  const choicesCount = arr.reduce((choicesCount, choice) => {
+    choicesCount[choice] = ++choicesCount[choice] || 1;
+
+    return choicesCount;
+  }, {});
+
+  return (choicesCount[mostCommon] > 1) ? true : false;
+}
+
+function playRandom() {
+  console.log('I\'m feeling a little crazy today.');
+  const randomIndex = Math.floor(Math.random() * CHOICES.length);
+  return CHOICES[randomIndex];
 }
